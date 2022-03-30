0459dfc6c8ff73a69b3a612935f46511db314617
geekhenno <henno.noman@gmail.com>
2022-03-30 16:57:22 +0300

    update example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:extended_phone_number_input/consts/enums.dart';
 import 'package:extended_phone_number_input/phone_number_input.dart';
 import 'package:flutter/material.dart';
 
@@ -12,20 +13,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Phone Number Input'),
     );
   }
 }
@@ -39,20 +30,88 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-              PhoneNumberInput(initialCountry: 'SA', locale: 'it'),
-          ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 20,
+              ),
+              const Text('bottom sheet mode'),
+              const PhoneNumberInput(
+                initialCountry: 'SA',
+                locale: 'it',
+                countryListMode: CountryListMode.bottomSheet,
+                contactsPickerPosition: ContactsPickerPosition.suffix,
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              const Text('dialog mode'),
+              const PhoneNumberInput(
+                initialCountry: 'SA',
+                locale: 'it',
+                countryListMode: CountryListMode.dialog,
+                contactsPickerPosition: ContactsPickerPosition.suffix,
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              const Text('custom border'),
+              PhoneNumberInput(
+                initialCountry: 'TN',
+                locale: 'it',
+                countryListMode: CountryListMode.dialog,
+                contactsPickerPosition: ContactsPickerPosition.suffix,
+                enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: const BorderSide(color: Colors.purple)),
+                focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: const BorderSide(color: Colors.purple)),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              const Text('bottom picker widget with custom widget'),
+              const PhoneNumberInput(
+                initialCountry: 'YE',
+                locale: 'it',
+                countryListMode: CountryListMode.dialog,
+                contactsPickerPosition: ContactsPickerPosition.bottom,
+                pickContactIcon: Card(
+                  color: Colors.blueGrey,
+                  child: Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: Text(
+                      'select from contacts',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              const Text('custom picker icon , no flag, hint'),
+              const PhoneNumberInput(
+                locale: 'it',
+                countryListMode: CountryListMode.bottomSheet,
+                contactsPickerPosition: ContactsPickerPosition.suffix,
+                pickContactIcon: Icon(Icons.add),
+                showSelectedFlag: false,
+                hint: 'XXXXXXXXXXX',
+              ),
+            ],
+          ),
         ),
       ),
     );
